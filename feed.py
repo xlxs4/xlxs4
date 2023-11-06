61892ec3dd5c3df90b9cc2be5935e0495dc208ca
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-11-06 19:48:44 +0200

    Try parse from HTML

--- feed.py
+++ feed.py
@@ -2,6 +2,7 @@ import re
 from os import path
 
 import requests
+from bs4 import BeautifulSoup
 
 
 def open_readme():
@@ -31,17 +32,29 @@ def modify_readme(readme, text, identifier=''):
 def fetch_posts(url):
     response = requests.get(url)
     response.raise_for_status()
-    posts_json = response.json()
-
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    def clean_date(date_str):
+        match = re.search(r'\d{4}-\d{2}-\d{2}', date_str)
+        return match.group(0) if match else "No date"
     posts = []
-    for post in posts_json[-3:]:
-        posts.append(f"- {post['title']}")
+    for link in soup.find_all('a', attrs={'aria-label': lambda v: v and v.startswith('post link to')}):
+        title = link['aria-label'].replace('post link to ', '')
+        url = link['href']
+        # Navigate to the parent and find the previous sibling that contains the date
+        date_container = link.find_previous_sibling(class_='archive-meta')
+        if date_container and date_container.span:
+            date_str = date_container.span.get('title', '')
+            date = clean_date(date_str)
+        else:
+            date = "No date"
+        posts.append(f"- [{title}]({url}) - {date}")
     return posts
 
 
 def main():
     n_last_posts = 3
-    posts = fetch_posts("https://xlxs4.com/index.json")
+    posts = fetch_posts("https://xlxs4.com/archives/")
     last_posts = '\n' + '\n'.join(posts[:n_last_posts]) + '\n'
     original = open_readme()
     updated = modify_readme(original, last_posts, identifier='BLOG')
